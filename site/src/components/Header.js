50a510102631ac1da52df3ea3688b9f87d358bb7
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-08-30 23:38:41 +0300

    Add new design

--- site/src/components/Header.js
+++ site/src/components/Header.js
@@ -1,33 +1,56 @@
 export default function Header() {
     return (
-        <nav class="navbar navbar-expand-lg custom_nav-container ">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <nav className="navbar navbar-expand-lg bg-white shadow-lg fixed-top">
+            <div className="container">
+                <a className="navbar-brand" href="#section_1">
+                    <i className="navbar-brand-icon bi-book me-2"></i>
+                    <span>ebook</span>
+                </a>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <div class="d-flex  flex-column flex-lg-row align-items-center">
-                    <ul class="navbar-nav  ">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                <div className="d-lg-none ms-auto me-3">
+                    <a href="#" className="btn custom-btn custom-border-btn btn-naira btn-inverted">
+                        <i className="btn-icon bi-cloud-download"></i>
+                        <span>Download</span>
+                    </a>
+                </div>
+
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-lg-auto me-lg-4">
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#section_1">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="about.html">About </a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#section_2">The Book</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="service.html">Services </a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#section_3">Author</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="contact.html">Contact Us</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#section_4">Reviews</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Login</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#section_5">Contact</a>
                         </li>
                     </ul>
-                    <form class="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
-                        <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
-                    </form>
+
+                    <div className="d-none d-lg-block">
+                        <a href="#" className="btn custom-btn custom-border-btn btn-naira btn-inverted">
+                            <i className="btn-icon bi-cloud-download"></i>
+                            <span>Download</span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </nav>
--- site/src/components/Home.js
+++ site/src/components/Home.js
@@ -1,147 +1,69 @@
-import Header from "./Header";
-
 export default function Home() {
+    const handleScroll = (e, id) => {
+        e.preventDefault();
+        const target = document.querySelector(id);
+        if (target) {
+            target.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
     return (
         <>
-            <div class="hero_area">
-                <header class="header_section">
-                    <div class="container">
-                        <nav class="navbar navbar-expand-lg custom_nav-container">
-                            <a class="navbar-brand" href="index.html">
-                                <img src="images/logo.png" alt="" />
-                                <span>
-                                    Energym
-                                </span>
-                            </a>
-                            <div class="contact_nav" id="">
-                                <ul class="navbar-nav ">
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="service.html">
-                                            <img src="images/location.png" alt="" />
-                                            <span>Location</span>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="service.html">
-                                            <img src="images/call.png" alt="" />
-                                            <span>Call : + 01 1234567890</span>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="service.html">
-                                            <img src="images/envelope.png" alt="" />
-                                            <span>demo@gmail.com</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
-                    </div>
+            <section className="hero-section d-flex justify-content-center align-items-center" id="home">
+                <div className="container">
+                    <div className="row">
 
-                </header>
-                <section class=" slider_section position-relative">
-                    <div class="container">
-                        <div class="custom_nav2">
-                            <Header />
+                        <div className="col-lg-6 col-12 mb-5 pb-5 pb-lg-0 mb-lg-0">
+
+                            <h6>Introducing ebook</h6>
+
+                            <h1 className="text-white mb-4">
+                                ebook landing page for professionals
+                            </h1>
+
+                            <a
+                                href="#discover"
+                                onClick={(e) => handleScroll(e, "#discover")}
+                                className="btn custom-btn me-3"
+                            >
+                                Discover More
+                            </a>
+
+                            <a
+                                href="#author"
+                                onClick={(e) => handleScroll(e, "#author")}
+                                className="link link--kale"
+                            >
+                                Meet the Author
+                            </a>
                         </div>
-                    </div>
-                    <div class="slider_container">
-                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <div class="container">
-                                        <div class="row">
-                                            <div class="col-lg-6 col-md-7 offset-md-6 offset-md-5">
-                                                <div class="detail-box">
-                                                    <h2>
-                                                        Get Your Body
-                                                    </h2>
-                                                    <h1>
-                                                        Fitness Here
-                                                    </h1>
-                                                    <p>
-                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                                        labore et dolore magna aliqua. Ut enim ad minim veniam
-                                                    </p>
-                                                    <div class="btn-box">
-                                                        <a href="" class="btn-1">
-                                                            Read More
-                                                        </a>
-                                                        <a href="" class="btn-2">
-                                                            Get A Quote
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="carousel-item ">
-                                    <div class="container">
-                                        <div class="row">
-                                            <div class="col-lg-6 col-md-7 offset-md-6 offset-md-5">
-                                                <div class="detail-box">
-                                                    <h2>
-                                                        Get Your Body
-                                                    </h2>
-                                                    <h1>
-                                                        Fitness Here
-                                                    </h1>
-                                                    <p>
-                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                                        labore et dolore magna aliqua. Ut enim ad minim veniam
-                                                    </p>
-                                                    <div class="btn-box">
-                                                        <a href="" class="btn-1">
-                                                            Read More
-                                                        </a>
-                                                        <a href="" class="btn-2">
-                                                            Get A Quote
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="carousel-item ">
-                                    <div class="container">
-                                        <div class="row">
-                                            <div class="col-lg-6 col-md-7 offset-md-6 offset-md-5">
-                                                <div class="detail-box">
-                                                    <h2>
-                                                        Get Your Body
-                                                    </h2>
-                                                    <h1>
-                                                        Fitness Here
-                                                    </h1>
-                                                    <p>
-                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                                        labore et dolore magna aliqua. Ut enim ad minim veniam
-                                                    </p>
-                                                    <div class="btn-box">
-                                                        <a href="" class="btn-1">
-                                                            Read More
-                                                        </a>
-                                                        <a href="" class="btn-2">
-                                                            Get A Quote
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+
+                        <div className="hero-image-wrap col-lg-6 col-12 mt-3 mt-lg-0">
+                            <img
+                                src="images/education-online-books.png"
+                                className="hero-image img-fluid"
+                                alt="education online books"
+                            />
                         </div>
+
                     </div>
-                </section>
-            </div>
+                </div>
+            </section>
+
+            {/* Dummy sections за навигация */}
+            <section id="discover" style={{ height: "100vh", background: "#f5f5f5" }}>
+                <div className="container">
+                    <h2>Discover More</h2>
+                    <p>Some content about the ebook goes here...</p>
+                </div>
+            </section>
+
+            <section id="author" style={{ height: "100vh", background: "#ddd" }}>
+                <div className="container">
+                    <h2>Meet the Author</h2>
+                    <p>Author biography and details...</p>
+                </div>
+            </section>
         </>
     );
 }
