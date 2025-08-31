42df794c2e272ddd2a923a70949c5a65f874b02e
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-08-31 22:48:19 +0300

    Add login form

--- site/src/components/Login/Login.js
+++ site/src/components/Login/Login.js
@@ -3,15 +3,18 @@ import './Login.css';
 export default function Login() {
     return (
         <>
-            <div class="login-box">
-                <h2>Login</h2>
-                <form>
-                    <input type="email" placeholder="Email" required />
-                    <input type="password" placeholder="Password" required />
-                    <button type="submit">Sign In</button>
-                </form>
-                <p>Don't have an account? <a href="#">Sign up</a></p>
-            </div>
+            <section class="login-section">
+                <div class="login-box">
+                    <h2>Login</h2>
+                    <form>
+                        <input type="email" placeholder="Email" required />
+                        <input type="password" placeholder="Password" required />
+                        <button type="submit">Sign In</button>
+                    </form>
+                    <p>Don't have an account? <a href="#">Sign up</a></p>
+                </div>
+            </section>
+
         </>
     );
 }
