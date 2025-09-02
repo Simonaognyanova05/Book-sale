37e755a6f87387f212fb57d1cd5b87acea031471
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-09-02 22:39:23 +0300

    Add dynamic header

--- site/src/components/Header.js
+++ site/src/components/Header.js
@@ -1,4 +1,31 @@
+import { Link } from 'react-router-dom';
+
 export default function Header() {
+
+    const unlogged = (
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav mu-menu navbar-right">
+                <li><a href="#">HOME</a></li>
+                <li><a href="#mu-book-overview">OVERVIEW</a></li>
+                <li><a href="#mu-author">AUTHOR</a></li>
+                <li><a href="#mu-pricing">PRICE</a></li>
+                <li><a href="#mu-testimonials">TESTIMONIALS</a></li>
+                <li><a href="#mu-contact">CONTACT</a></li>
+            </ul>
+        </div>
+    );
+
+    const logged = (
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav mu-menu navbar-right">
+                <li><Link to="/editHome">HOME</Link></li>
+                <li><Link to="/editOverview">OVERVIEW</Link></li>
+                <li><Link to="/editAuthor">AUTHOR</Link></li>
+                <li><Link to="/editPrices">PRICE</Link></li>
+                <li><Link to="#mu-contact">CONTACT</Link></li>
+            </ul>
+        </div>
+    );
     return (
         <>
             <header id="mu-header" class="" role="banner">
@@ -13,7 +40,7 @@ export default function Header() {
                                     <span class="icon-bar"></span>
                                 </button>
 
-                                <a class="navbar-brand" href="index.html"><i class="fa fa-book" aria-hidden="true"></i> Kindle</a>
+                                <Link class="navbar-brand" to="/"><i class="fa fa-book" aria-hidden="true"></i> Kindle</Link>
 
 
 
