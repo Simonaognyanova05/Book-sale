1fd45809fac0ac89d8b0cc839173b0287ea8e5c8
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-09-06 09:42:38 +0300

    Add edit prices form

--- site/src/components/PricesForm/PricesForm.js
+++ site/src/components/PricesForm/PricesForm.js
@@ -1,17 +1,45 @@
+import { updatePrices } from '../../services/updates/updatePrices';
 import './PricesForm.css';
+import { useState } from 'react';
 
 export default function PricesForm() {
+    const [formData, setFormData] = useState({
+        description: "",
+        orderType: "",
+        priceLv: "",
+        priceEuro: "",
+        descriptionOffer: "",
+    });
+
+    // обновява state при промяна в инпутите
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    // изпраща към Firebase
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await updatePrices(formData); // извиква сървиза
+    };
     return (
         <>
             <section class="login-section">
                 <div class="login-box">
                     <h2>Редактиране на страница "Цени"</h2>
-                    <form>
-                        <input type="text" name="description" placeholder="Описание" required />
-                        <input type="text" name="orderType" placeholder="Тип поръчка" required />
-                        <input type="text" name="priceLv" placeholder="Цена в лв." required />
-                        <input type="text" name="priceEuro" placeholder="Цена в евро" required />
-                        <input type="text" name="descriptionOffer" placeholder="Описание на офертата" required />
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" name="description" placeholder="Описание" value={formData.description}
+                            onChange={handleChange} required />
+                        <input type="text" name="orderType" placeholder="Тип поръчка" value={formData.orderType}
+                            onChange={handleChange} required />
+                        <input type="text" name="priceLv" placeholder="Цена в лв." value={formData.priceLv}
+                            onChange={handleChange} required />
+                        <input type="text" name="priceEuro" placeholder="Цена в евро" value={formData.priceEuro}
+                            onChange={handleChange} required />
+                        <input type="text" name="descriptionOffer" placeholder="Описание на офертата" value={formData.descriptionOffer}
+                            onChange={handleChange} required />
                         <button type="submit">Редактирай</button>
                     </form>
                 </div>
@@ -19,4 +47,4 @@ export default function PricesForm() {
 
         </>
     );
-}
+} 
---
+++ site/src/services/updates/updatePrices.js
@@ -0,0 +1,23 @@
+// services/updateHomeInfo.js
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../config/firebase";
+
+export async function updatePrices(data) {
+    try {
+        // тук трябва да сложиш точния ID на документа от Firebase
+        const homeRef = doc(db, "prices", "b8s5qXMBC2CGDpzyIdPV");
+
+        await updateDoc(homeRef, {
+            description: data.description,
+            orderType: data.orderType,
+            priceLv: data.priceLv,
+            priceEuro: data.priceEuro,
+            descriptionOffer: data.descriptionOffer,
+
+        });
+
+        alert("Трета страница беше успешно обновена.");
+    } catch (error) {
+        alert("Грешка при обновяването: " + error.message);
+    }
+}
