f7417db29e6f28d07569aa902a4af3219ae867ea
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-09-06 09:34:28 +0300

    Add edit author form

--- site/src/components/AuthorForm/AuthorForm.js
+++ site/src/components/AuthorForm/AuthorForm.js
@@ -1,18 +1,53 @@
+import { updateAuthor } from '../../services/updates/updateAuthor';
 import './AuthorForm.css';
+import { useState } from 'react';
+
 
 export default function AuthorForm() {
+    const [formData, setFormData] = useState({
+        names: "",
+        description: "",
+        signature: "",
+        facebook: "",
+        instagram: "",
+        linkedIn: "",
+        img: "",
+    });
+
+    // обновява state при промяна в инпутите
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    // изпраща към Firebase
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await updateAuthor(formData); // извиква сървиза
+    };
     return (
         <>
             <section class="login-section">
                 <div class="login-box">
                     <h2>Редактиране на страница "За автора"</h2>
-                    <form>
-                        <input type="text" name="names" placeholder="Имена" required />
-                        <input type="text" name="description" placeholder="Описание" required />
-                        <input type="text" name="signature" placeholder="Подпис (линк към снимка)" required />
-                        <input type="text" name="facebook" placeholder="Линк към facebook" required />
-                        <input type="text" name="linkedIn" placeholder="Линк към LinkedIn" required />
-                        <input type="text" name="instagram" placeholder="Линк към Instagram" required />
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" name="names" placeholder="Имена" value={formData.names}
+                            onChange={handleChange} required />
+                        <input type="text" name="description" placeholder="Описание" value={formData.description}
+                            onChange={handleChange} required />
+                        <input type="text" name="signature" placeholder="Подпис (линк към снимка)" value={formData.signature}
+                            onChange={handleChange} required />
+                        <input type="text" name="facebook" placeholder="Линк към facebook" value={formData.facebook}
+                            onChange={handleChange} required />
+                        <input type="text" name="linkedIn" placeholder="Линк към LinkedIn" value={formData.linkedIn}
+                            onChange={handleChange} required />
+                        <input type="text" name="instagram" placeholder="Линк към Instagram" value={formData.instagram}
+                            onChange={handleChange} required />
+                        <input type="text" name="img" placeholder="Линк към изображение" value={formData.img}
+                            onChange={handleChange} required />
+
                         <button type="submit">Редактирай</button>
                     </form>
                 </div>
---
+++ site/src/services/updates/updateAuthor.js
@@ -0,0 +1,25 @@
+// services/updateHomeInfo.js
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../config/firebase";
+
+export async function updateAuthor(data) {
+    try {
+        // тук трябва да сложиш точния ID на документа от Firebase
+        const homeRef = doc(db, "author", "vtZYpW5UUf1KCxKREAAH");
+
+        await updateDoc(homeRef, {
+            names: data.names,
+            description: data.description,
+            signature: data.signature,
+            facebook: data.facebook,
+            instagram: data.instagram,
+            linkedIn: data.linkedIn,
+            img: data.img,
+
+        });
+
+        alert("Трета страница беше успешно обновена.");
+    } catch (error) {
+        alert("Грешка при обновяването: " + error.message);
+    }
+}
